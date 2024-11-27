f87c69c71dcbcfb31b64d6911f5a0f99a246e7e5
Okinea Dev <okineadev@users.noreply.github.com>
2024-11-27 21:51:48 +0200

    chore: add link with solution for Vue components resolution
    https://github.com/vuejs/vue-cli/issues/1198#issuecomment-1139752067

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,7 @@
+// https://github.com/vuejs/vue-cli/issues/1198#issuecomment-1139752067
+
 declare module '*.vue' {
-	import { DefineComponent } from 'vue';
-	const component: DefineComponent<{}, {}, any>;
+	import type { DefineComponent } from 'vue';
+	const component: DefineComponent;
 	export default component;
 }
